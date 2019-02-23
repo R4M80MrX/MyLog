0b88d5cfc45e9fcea9a88254b19dcf31e1ea8260
R4M80MrX <mrx@trappeddragon.com>
2019-02-23 12:12:50 +0800

    first

---
+++ core/log.py
@@ -0,0 +1,40 @@
+import logging
+import sys
+
+PATH = "log.txt"
+LOGGER = logging.getLogger(PATH)
+
+LOGGER_HANDLER = None
+try:
+    from thirdparty.ansistrm.ansistrm import ColorizingStreamHandler
+
+    disableColor = False
+
+    for argument in sys.argv:
+        if "disable-col" in argument:
+            disableColor = True
+            break
+
+    FILE_HANDLER = logging.FileHandler(PATH)
+
+    if disableColor:
+        LOGGER_HANDLER = logging.StreamHandler(sys.stdout)
+    else:
+        LOGGER_HANDLER = ColorizingStreamHandler(sys.stdout)
+except ImportError as e:
+    print(e)
+    LOGGER_HANDLER = logging.StreamHandler(sys.stdout)
+
+FORMATTER1 = logging.Formatter(
+    '\r[%(asctime)s] [%(levelname)s] %(message)s', '%H:%M:%S')
+FORMATTER2 = logging.Formatter(
+    '[%(asctime)s] [%(levelname)s] %(message)s', '%H:%M:%S')
+
+
+LOGGER_HANDLER.setFormatter(FORMATTER1)
+LOGGER.addHandler(LOGGER_HANDLER)
+LOGGER.setLevel(logging.DEBUG)
+
+FILE_HANDLER.setFormatter(FORMATTER2)
+LOGGER.addHandler(FILE_HANDLER)
+FILE_HANDLER.setLevel(logging.ERROR)
